415a88ab85498b5b2c1b7e1384fad5371f78860e
AndressaMac <105513933+AndressaMac@users.noreply.github.com>
2022-07-20 16:14:04 -0300

    texto salbo js

--- textSalvo.js
+++ textSalvo.js
@@ -4,12 +4,15 @@ const texto1 = document.querySelector('#textoSalvo')
 
 function carregar() {
   let saved = sessionStorage.getItem('text')
-  let list = JSON.parse(saved)
-  
-  texto1.innerHTML = "O texto aparece aqui" + list
+  let list = JSON.parse(saved) // transforma string em json
+  // "{"cahve": "valor"}" => {chave: 'valor'}
+//  let stringfy = JSON.stringify(list[list.length - 1]) transforma objeto em string
+    // {chave: 'valor'} => "{"cahve": "valor"}"
+
+  texto1.innerHTML = list.data + '    ' + list.text
 }
 
-function aqui(){
+function aqui() {
   console.log('oiiiii')
   carregar()
 }
